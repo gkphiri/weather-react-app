423b568d638291db9a5f361df0af5447e966a880
gkphiri <166555047+gkphiri@users.noreply.github.com>
2024-07-01 10:43:27 +0200

    Added App.js and index.js

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="mailto:kphiri68@gmail.com" target="_blank">
+            Grace Phiri
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/gkphiri/react-weather-app.git"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
